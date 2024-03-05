85b58fbc2036c1731f3ea50ae3f1d1c346366002
andresfacundo <andresfelipefacundo@gmail.com>
2024-03-05 13:55:22 -0500

    Añadido constantes

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,31 +1,34 @@
-import React, { useState } from 'react'
-import './inputCss.css'
+import React, { useRef, useState } from 'react';
+import './inputCss.css';
 
-
-const homeworkBurn = ['Tarea Ingles', 'Organizar Habitación', 'Organizar Cuarto']
+const homeworkBurn = ['Tarea Ingles', 'Organizar Habitación', 'Organizar Cuarto'];
 
 export const Input = () => {
-    const [tasks, setTasks] = useState(homeworkBurn);
+    
+  const inputReference = useRef('');
+
+  const handleSetTask = () => {
+    let response = inputReference.current.value;
+    console.log(response);
+  };
+
+  const [tasks, setTasks] = useState(homeworkBurn);
+
   return (
     <div>
-        <label>
-        <input ref='inputRef' className='homework-input' placeholder='Ingrese la Tarea' type="text" />
+      <label>
+        <input ref={inputReference} className='homework-input' placeholder='Ingrese la Tarea' type="text" />
       </label>
       <button onClick={handleSetTask}> Crear Tarea</button>
       <hr />
 
-    <div className='lists-items-div'>
-    <ul>
-    {tasks.map((task, index) => (
-         <li key={index}>{task}</li>
-    ))}
-
+      <div className='lists-items-div'>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
         </ul>
+      </div>
     </div>
-
-    </div>
-
-
-  )
-}
-
+  );
+};
